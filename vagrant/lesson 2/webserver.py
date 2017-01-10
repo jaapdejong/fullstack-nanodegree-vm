100581f15e128cd02e7920b3fc6184acf95fc42f
Jaap de Jong <jaap.dejong@nedap.com>
2017-01-10 20:51:48 +0100

    Add lesson 2 quiz 3 - POST

--- vagrant/lesson 2/webserver.py
+++ vagrant/lesson 2/webserver.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-
+import cgi
 
 class WebServerHandler(BaseHTTPRequestHandler):
 
@@ -10,24 +10,70 @@ class WebServerHandler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
-            message = ""
-            message += "<html><body>Hello!</body></html>"
-            self.wfile.write(message)
-            print message
+            output = ""
+            output += "<html>"
+            output += " <body>"
+            output += "  Hello!"
+            output += "  <form method='POST' enctype='multipart/form-data' action='hello'>"
+            output += "   <h2>What would you like me to say?</h2>"
+            output += "   <input name='message' type='text'>"
+            output += "   <input type='submit' value='Submit'>"
+            output += "  </form>"
+            output += " </body>"
+            output += "</html>"
+            self.wfile.write(output)
+            print output
             return
 
         if self.path.endswith("/hola"):
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
-            message = ""
-            message += "<html><body> &#161 Hola ! </body></html>"
-            self.wfile.write(message)
-            print message
+            output = ""
+            output += "<html>"
+            output += " <body>"
+            output += "  &#161 Hola !"
+            output += "  <form method='POST' enctype='multipart/form-data' action='hello'>"
+            output += "   <h2>What would you like me to say?</h2>"
+            output += "   <input name='message' type='text'>"
+            output += "   <input type='submit' value='Submit'>"
+            output += "  </form>"
+            output += " </body>"
+            output += "</html>"
+            self.wfile.write(output)
+            print output
             return
 
         else:
             self.send_error(404, 'File Not Found: %s' % self.path)
+
+    def do_POST(self):
+        try:
+            self.send_response(301)
+            self.end_headers()
+            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+            if ctype == 'multipart/form-data':
+                fields = cgi.parse_multipart(self.rfile, pdict)
+                messagecontent = fields.get('message')
+            
+            output = ""
+            output += "<html>"
+            output += " <body>"
+            output += "  <h2> Okay, how about this: </h2>"
+            output += "  <h1> %s </h1>" % messagecontent[0]
+            output += "  <form method='POST' enctype='multipart/form-data' action='hello'>"
+            output += "   <h2>What would you like me to say?</h2>"
+            output += "   <input name='message' type='text'>"
+            output += "   <input type='submit' value='Submit'>"
+            output += "  </form>"
+            output += " </body>"
+            output += "</html>"
+            self.wfile.write(output)
+            print output
+            return
+            
+        except:
+            pass
 
 
 def main():
