cec73986ee3eedcad3cf0ed6729c57d25c137b5c
Jaap de Jong <jaap.dejong@nedap.com>
2017-01-10 21:11:07 +0100

    Add lesson 2 objective 1

---
+++ vagrant/lesson 2/webserver-1.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import cgi
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
+class WebServerHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        if self.path.endswith("/restaurants"):
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            output = ""
+            output += "<html>"
+            output += " <body>"
+
+            restaurants = session.query(Restaurant).all()
+            for restaurant in restaurants:
+                output += restaurant.name
+                output += "</br>"
+
+            output += " </body>"
+            output += "</html>"
+            self.wfile.write(output)
+            print output
+            return
+
+        else:
+            self.send_error(404, 'File Not Found: %s' % self.path)
+
+def main():
+    try:
+        port = 8080
+        server = HTTPServer(('', port), WebServerHandler)
+        print "Web Server running on port %s" % port
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print " ^C entered, stopping web server...."
+        server.socket.close()
+
+if __name__ == '__main__':
+    main()
